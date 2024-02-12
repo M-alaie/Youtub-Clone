da4c10b0deef96fe97b1887c0dbd7bc6f00ad6b0
morteza alaienasab <alaienasabmorteza3@gmail.com>
2024-02-12 19:29:17 +0330

    finish main section and i want to responsive for other devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 
 const menu=document.querySelector('#menu')
 const Aside=document.querySelector('aside')
+const Container=document.querySelector('.container')
 
 menu.onclick=()=>{
     Aside.classList.toggle('small_aside')
+    Container.classList.toggle('small_container')
 }
